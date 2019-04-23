6489fb79f33f27f47b1e04eeb6ccedee17d0c066
Pratyush Singh <singh.pratyush96@gmail.com>
2019-04-23 11:58:39 +0530

    Fix base route

---
+++ steem/precache-manifest.f72489e70d25a759d75c963f4e44bc1d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "e02e9520868e3d03b53797200caf16b7",
+    "url": "/steem/static/media/featureImage1.e02e9520.gif"
+  },
+  {
+    "revision": "15cd6a9c539d43a31aff",
+    "url": "/steem/static/css/main.f08278de.chunk.css"
+  },
+  {
+    "revision": "c2df241ed1acb0e81eee",
+    "url": "/steem/static/js/runtime~main.c2df241e.js"
+  },
+  {
+    "revision": "fdac574b31ccfe58aaebe14375a8c66f",
+    "url": "/steem/static/media/ecotrain.fdac574b.png"
+  },
+  {
+    "revision": "b619c6f56bec4b9edfe6",
+    "url": "/steem/static/js/2.b619c6f5.chunk.js"
+  },
+  {
+    "revision": "7afaefae370f1a52596acc413ae0b306",
+    "url": "/steem/static/media/featureImage2.7afaefae.gif"
+  },
+  {
+    "revision": "752dbd7c91047565d207a169697f1835",
+    "url": "/steem/static/media/vikram_hapramp.752dbd7c.jpg"
+  },
+  {
+    "revision": "5169dcad50bbf37ca7f2f1ca8e5b0157",
+    "url": "/steem/static/media/pratyush_1_hapramp.5169dcad.jpg"
+  },
+  {
+    "revision": "958a01f704004193f4e7f6387c195b35",
+    "url": "/steem/static/media/ankit_hapramp.958a01f7.jpg"
+  },
+  {
+    "revision": "ed268187a3f1fa3f9e5d695e83023004",
+    "url": "/steem/static/media/mofid_hapramp.ed268187.jpg"
+  },
+  {
+    "revision": "b96f5b57a10498d263727292bbc6f59d",
+    "url": "/steem/static/media/rajat_hapramp.b96f5b57.jpg"
+  },
+  {
+    "revision": "abe37fcd0e8a3d1edb8515468269c5a3",
+    "url": "/steem/static/media/heroImage.abe37fcd.gif"
+  },
+  {
+    "revision": "15cd6a9c539d43a31aff",
+    "url": "/steem/static/js/main.15cd6a9c.chunk.js"
+  },
+  {
+    "revision": "2d384fe048baf158375dbd96e55ca796",
+    "url": "/steem/static/media/featureImage3.2d384fe0.gif"
+  },
+  {
+    "revision": "848c5f95dc7284068f3a253dc6a3c9da",
+    "url": "/steem/static/media/heroAbout.848c5f95.png"
+  },
+  {
+    "revision": "3f81fa29fe7f3ae9495b1eb9b6788153",
+    "url": "/steem/static/media/huddle.3f81fa29.png"
+  },
+  {
+    "revision": "b3b033e3369af12e560d86f5f38ac462",
+    "url": "/steem/static/media/nascomm_logo.b3b033e3.png"
+  },
+  {
+    "revision": "2d997966305da87c046a3f98bbc90250",
+    "url": "/steem/static/media/iiitv.2d997966.png"
+  },
+  {
+    "revision": "ab834ecf2363edfab1f98c25441c7fed",
+    "url": "/steem/static/media/foxtales.ab834ecf.png"
+  },
+  {
+    "revision": "21c2ff02c5288a7b7ed619b690415358",
+    "url": "/steem/static/media/freewrite.21c2ff02.jpeg"
+  },
+  {
+    "revision": "2b2a588f963915ac9a53d8ccbb8d6627",
+    "url": "/steem/static/media/crowdmind.2b2a588f.png"
+  },
+  {
+    "revision": "677b91d0922b3c7977be4a755c31d6b4",
+    "url": "/steem/static/media/photomag.677b91d0.png"
+  },
+  {
+    "revision": "b619c6f56bec4b9edfe6",
+    "url": "/steem/static/css/2.c2a0af3d.chunk.css"
+  },
+  {
+    "revision": "ab52799d18b73160281eebe741c51ee2",
+    "url": "/steem/index.html"
+  }
+];
--- steem/service-worker.js
+++ steem/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.056b5da9b8e498cbe856192975999a02.js"
+  "/steem/precache-manifest.f72489e70d25a759d75c963f4e44bc1d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/steem/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
